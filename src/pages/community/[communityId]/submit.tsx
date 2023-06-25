57179e76677abd2239c7be9d7066970abb52fa4a
Bipin Raj Sitoula <viraatsitoula@gmail.com>
2023-06-26 09:18:17 +1000

    Changes to Comments Component

--- src/pages/community/[communityId]/submit.tsx
+++ src/pages/community/[communityId]/submit.tsx
@@ -1,4 +1,3 @@
-import { communityState } from "@/atoms/communitiesAtom";
 import About from "@/components/Community/About";
 import PageContent from "@/components/Layout/PageContent";
 import NewPostForm from "@/components/Posts/NewPostForm";
@@ -7,13 +6,10 @@ import useCommunityData from "@/hooks/useCommunityData";
 import { Box, Text } from "@chakra-ui/react";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useRecoilValue } from "recoil";
 
 const SubmitPostPage: React.FC = () => {
   const [user] = useAuthState(auth);
-
   const { communityStateValue } = useCommunityData();
-  console.log("Community", communityStateValue);
 
   return (
     <PageContent>
